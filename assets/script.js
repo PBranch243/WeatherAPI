a6e7d33f6b0fdd39f185672c82b78c4b1d5a67b9
Phil Branch <philbranch243@gmail.com>
2022-06-16 18:11:36 -0500

    working on 2nd call

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 // Declare variables
-
-
+var cityLat = "";
+var cityLon = "";
+var myLink = "";
 
 
 //add listener to search button
@@ -9,19 +10,28 @@ $("#newCity").on("click", function () {
     var value = $(this).siblings("input").val();
     console.log(value)
 
-    //fetch data from weather API
+    //fetch data from weather 
 
     //make first call to get lat & lon
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q="+value+"&limit=1&appid=2b9bc0455fe9f94b38ffcf663b546cfe")
-    .then(function(data){
-        return data.json();
-    
-    }).then(function(data){
-        console.log(data[0].lat)
-        //2nd call to other API
-    })
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + value + "&limit=1&appid=2b9bc0455fe9f94b38ffcf663b546cfe")
+        .then(function (data) {
+            return data.json();
+
+        }).then(function (data) {
+            cityLat = data[0].lat;
+            cityLon = data[0].lon;
+            myLink = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&exclude={part}&appid=2b9bc0455fe9f94b38ffcf663b546cfe"
+
+            console.log(myLink);
+
+            //fetch("https://api.openweathermap.org/data/2.5/onecall?lat=cityLat&lon={lon}&exclude={part}&appid=2b9bc0455fe9f94b38ffcf663b546cfe")
+
+
+            // console.log(data[0].lat)
+            //2nd call to other API
+        })
     //2b9bc0455fe9f94b38ffcf663b546cfe   API KEY
-    //pass lat and lon in to make 2nd call
+
 
 })
 
